2e4ac8fb2e7e2e50857742e2858660013e0a0e39
SuperDev0601 <superdev0601@outlook.com>
2022-10-06 00:28:20 -0700

    dashboard page added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import { Button } from 'react-bootstrap';
 import { Entry } from './page/entry/Entry.page';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './page/entry/dashboard/Dashboard';
 function App() {
   return (
     <div className="App">
       {/*<Entry></Entry>*/}
       <DefaultLayout>
-        main page
+        <Dashboard />
       </DefaultLayout>
     </div>
   );
