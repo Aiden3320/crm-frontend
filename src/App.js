b26754886cbb170c9feeed375769d445a4d456b8
SuperDev0601 <superdev0601@outlook.com>
2022-10-05 23:30:33 -0700

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { Button } from 'react-bootstrap';
 import { Entry } from './page/entry/Entry.page';
+import { DefaultLayout } from './layout/DefaultLayout';
 function App() {
   return (
     <div className="App">
-      <Entry></Entry>
+      {/*<Entry></Entry>*/}
+      <DefaultLayout>
+        main page
+      </DefaultLayout>
     </div>
   );
 }
